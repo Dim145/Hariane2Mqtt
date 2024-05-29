3e472a1141b136333608f3989cbb35806f7f2b58
Dim145 <dimitri.bleach@gmail.com>
2024-05-29 18:17:30 +0200

    test

--- addon/entrypoint.sh
+++ addon/entrypoint.sh
@@ -7,26 +7,19 @@ if [ ! -d /config ]; then
 fi
 
 # Default location
-CONFIGSOURCE="/data/config.yaml"
+CONFIGSOURCE="/data/options.json"
 
-cat /data/options.json
 
-# transform each config.yaml entries to env variables
-if [ -f $CONFIGSOURCE ]; then
-    echo "Loading config from $CONFIGSOURCE"
-    while IFS=': ' read -r key value
-    do
-        if [ ! -z "$key" ] && [ ! -z "$value" ]; then
-            key=$(echo "$key" | tr '[:lower:]' '[:upper:]' | tr '-' '_' | tr '.' '_')
-            value=$(echo "$value" | sed 's/^ *//g' | sed 's/ *$//g')
-            
-            # add to env for cron jobs
-            export "$key=$value"
-            
-            # append to .env file
-            echo "export $key=$value;" >> /.env
-        fi
-    done < $CONFIGSOURCE
-fi
+# transform each options.json key to env var
+for key in $(jq -r 'keys[]' $CONFIGSOURCE); do
+    value=$(jq -r ".$key" $CONFIGSOURCE)
+    key=$(echo "$key" | tr '[:lower:]' '[:upper:]' | tr '-' '_')
+    
+    # add to env for cron jobs
+    export "$key=$value"
+    
+    # append to .env file
+    echo "export $key=$value;" >> /.env
+done
 
 exec "$@"
